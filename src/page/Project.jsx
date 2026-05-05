729da5678d9b08f169fc8ddba5da375affe86cfb
patelvishal12005 <patelvishal12005@gmail.com>
2026-05-05 19:58:05 +0530

    Update project files

--- src/page/Project.jsx
+++ src/page/Project.jsx
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react'
 import './Project.css'
 import Aos from 'aos'
 function Project() {
-  useEffect(()=>{
+  useEffect(() => {
     Aos.init({
-      duration:1500,
-      once:true
+      duration: 1500,
+      once: true
     })
   }, [])
   return (
     <div className="project" id="project">
 
-  {/* <video autoPlay loop muted playsInline className="video1">
+      {/* <video autoPlay loop muted playsInline className="video1">
         <source src="project.mp4" type="video/mp4" />
       </video> */}
       <h1 className="title" data-aos="fade-right">My Projects</h1>
@@ -19,7 +19,7 @@ function Project() {
       <div data-aos="fade-down" className="project-container">
 
         <div className="project-card">
-          <img data-aos="fade-left" src="logo1.png" alt="project"/>
+          <img data-aos="fade-left" src="logo1.png" alt="project" />
           <h3 data-aos="fade-right">Food</h3>
           <p data-aos="fade-left">React portfolio website with animations.</p>
           <a href="https://patelvishal12005-design.github.io/Food_ordering/" target="_blank" rel="noopener noreferrer">
@@ -27,20 +27,24 @@ function Project() {
           </a>
         </div>
 
-        {/* <div className="project-card">
-          <img src="p2.png" alt="project"/>
-          <h3>Todo App</h3>
-          <p>Task management app using JavaScript.</p>
-          <button>View Project</button>
+        <div className="project-card">
+          <img data-aos="fade-left" src="om-logo.svg" alt="Hari Om Engineering Works" height={100} />
+          <h3 data-aos="fade-right">Hari Om Engineering</h3>
+          <p data-aos="fade-left">Company website for Hari Om Engineering Works.</p>
+          <a href="https://patelvishal12005-design.github.io/Hari_om/" target="_blank" rel="noopener noreferrer">
+            <button data-aos="fade-up" data-aos-delay="500">View Project</button>
+          </a>
         </div>
 
-        <div className="project-card">
-          <img src="p3.png" alt="project"/>
-          <h3>E-Commerce UI</h3>
-          <p>Responsive shopping website design.</p>
+       <div className="project-card">
+          <img src="p.png" alt="project"/>
+          <h3>ERP</h3>
+          <h5>Enterprise Resource Planning</h5>
+          <p>ERP for Enterprise Resource Planning</p>
+        <a href="https://patelvishal12005-design.github.io/erp-frontend/" target="_blank" rel="noopener noreferrer">
           <button>View Project</button>
-        </div> */}
-
+        </a>
+        </div>
       </div>
 
     </div>
